c34e57b630294aa85cc3cbe6cb162211802ae110
Chelle <chellecol@gmail.com>
2020-11-23 16:55:23 -0500

    setting up app and models

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, request
 import requests as req
-from random import randint
 
 app = Flask(__name__)
 
@@ -15,3 +14,23 @@ def homepage():
 @app.route('/<int:image_id>/edit')
 def edit(image_id):
     """Show edit page"""
+    return render_template('test.html', title='Edit Page Route')
+
+
+@app.route('/signup')
+def signup():
+    """Show the signup page"""
+    return render_template('test.html', title='Signup Route')
+
+@app.route('/login')
+def login():
+    """Show the login page"""
+    return render_template('test.html', title='Login Route')
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    """Log the current user out"""
+    return render_template('test.html', title='Logout Route')
+
+
--- models.py
+++ models.py
@@ -7,4 +7,44 @@ def connect_db(app):
     db.init_app(app)
 
 class User(db.Model):
+    """Store the user information, and provide sign up and authentification"""
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, nullable=False, unique=True)
+    password_hash = db.Column(db.Text, nullable=False)
+    bio = db.Column(db.Text)
+
+class Image(db.Model):
+    """Store the image information"""
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    url = db.Coulmn(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Filter(db.Model):
+    """Store the name and data of custom & default filters"""
+    __tablename__ = 'filters'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    full_name = db.Column(db.Text, nullable=false)
+    user_id = db.Column(db.Text, db.ForeignKey('users.id'))
+    brightness = db.Column(db.Text)
+    contrast = db.Column(db.Text)
+    saturation = db.Column(db.Text)
+
+class ImageFilter(db.Model):
+    """Connection of images and filters"""
+
+    __tablename__ = 'image_filters'
+
+    image_id = db.Column(
+        db.Integer,
+        db.ForeignKey('images.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    filter_id = db.Column(
+        db.Integer,
+        db.ForeignKey('filters.id', ondelete="cascade"),
+        primary_key=True,
+    )
