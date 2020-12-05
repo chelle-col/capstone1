332b18b1dcc9d1df505d718d23faf6373e4071d6
Chelle <chellecol@gmail.com>
2020-12-04 21:26:48 -0500

    sliders on edit page working

--- static/edit.js
+++ static/edit.js
@@ -1,16 +1,41 @@
 function drawPicture() {
     const canvas = document.getElementById("canvas");
     const canvasContext = canvas.getContext("2d");
-    const img = document.getElementById("image-id");
+    const img = document.getElementById("image");
     canvasContext.drawImage(img, 0, 0);
 }
 
+function handleSilderChange(evt){
+    sliderData = getSliderData();
+    console.log(sliderData)
+    Caman('#canvas', $('#image'), function(){
+        this.revert(false);
+        this.saturation(sliderData.saturation).vibrance(sliderData.vibrance)
+            .contrast(sliderData.contrast).exposure(sliderData.exposure)
+            .hue(sliderData.hue).sepia(sliderData.sepia).render();
+    })
+}
+
+function getSliderData(){
+    return {
+        'saturation' : $('#saturation').val(),
+        'vibrance' : $('#vibrance').val(),
+        'contrast' : $('#contrast').val(),
+        'exposure' : $('#exposure').val(),
+        'hue' : $('#hue').val(),
+        'sepia' : $('#sepia').val()
+    }
+}
+
 $(function() {
-    console.log( "ready!" );
     drawPicture();
     Caman('#canvas', function () {
-        this.brightness(20);
-        this.contrast(40);
+        console.log(this)
+        this.brightness(5);
         this.render();
     });
+    $('#sliders').change( (evt)=>{
+        console.log(evt.target)
+        handleSilderChange(evt);
+    })
 });
