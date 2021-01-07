2c5557aee2f1ad45d492056e41c156ebc2c3357a
Chelle <chellecol@gmail.com>
2021-01-07 15:57:34 -0500

    changed base url

--- static/base.js
+++ static/base.js
@@ -1,4 +1,4 @@
 
 // Base URL
-// const base_url = 'https://mycapstone1.herokuapp.com'
-const base_url = 'http://127.0.0.1:5000'
+const base_url = 'https://mycapstone1.herokuapp.com'
+// const base_url = 'http://127.0.0.1:5000'
